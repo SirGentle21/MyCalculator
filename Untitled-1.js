af34d820681a2c879faf30bed05c7fcd7f5bfdcb
SirGentle21 <briannzube5@gmail.com>
2023-04-20 23:05:44 +0100

    My Calculator commit

---
+++ Untitled-1.js
@@ -0,0 +1,57 @@
+console.log(5 + "5")
+console.log(5 - "5")
+console.log(3>2>1)
+
+var x = 2;
+console.log(x++)
+console.log(x)
+
+var x = 1;
+if(x==="1"){
+  console.log("Equal");
+} else {
+  console.log("Not equal")
+}
+
+var x= [1, 2, 3];
+console.log(x.length)
+
+var x = ["apple","banana","orange"];
+console.log(x[1])
+
+var x = "apple";
+console.log(x.lastIndexOf("p"))
+
+var x = [1, 2, 3];
+
+console.log(x.pop());
+
+console.log(x);
+
+function myFunction(a, b) {
+
+  return a + b;
+  
+  }
+  
+  console.log(myFunction(1, 2, 3));
+  
+  var x = [1, 2, 3];
+
+  x.push(4);
+  
+  console.log(x);
+  
+  var x = {
+
+    name: "John",
+    
+    age: 30,
+    
+    city: "New York"
+    
+    };
+    
+    console.log(x.age);    
+
+
